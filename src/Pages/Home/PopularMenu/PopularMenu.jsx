1acc621d82429bc9da190ed806d936debd8757d0
shahadat500441 <smd643925@gmail.com>
2025-09-19 23:06:33 +0600

    home page popularItem section added

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,9 +1,37 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { useState,useEffect } from 'react';
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
-    return (
-    <div>
+    const [menu, setMenu]=useState([])
 
+    useEffect(() => {
+   fetch("menu.json")
+   .then(res=> res.json())
+   .then(data=> {
+    const popularItems = data.filter(item=> item.category === "popular")
+    setMenu(popularItems)
+   })
+    
+    },[] )
+    
+    return (
+   <section>
+    <SectionTitle
+    subHeading={"---Check it out---"}
+    heading={"FROM OUR MENU"}
+    ></SectionTitle>
+
+    <div className="grid grid-cols-1 md:grid-cols-2  my-8 gap-5">
+        {
+            menu.map(item =><MenuItem
+            key={item._id}
+            item={item}
+            ></MenuItem>)
+        }
     </div>
+
+   </section>
     );
 };
 
---
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MenuItem = ({item}) => {
+    const {image,price,name,recipe}=item
+    return (
+        <div className="flex  space-x-6">
+            <img style={{borderRadius:"0px 200px 200px 200px"}} className="w-[120px] object-cover" src={image} alt="" />
+            <div>
+                <h2 className="text-xl font-semibold">{name}</h2>
+                <p>{recipe}</p>
+            </div>
+            <p className="text-yellow-400 font-medium">${price}</p>
+        </div>
+    );
+};
+
+export default MenuItem;

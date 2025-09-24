00d5f7791ae62588ccf3e6236b1083906a449272
shahadat500441 <smd643925@gmail.com>
2025-09-24 12:30:48 +0600

    signup page create user and authention done

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,7 +1,25 @@
 import {Link} from "react-router-dom"
-
-
+import { useForm } from "react-hook-form"
+import { AuthContext } from "../../provider/AuthProvider"
+import {useContext} from "react"
 const SignUp = () => {
+    const{createUser} = useContext(AuthContext)
+
+     const {
+    register,
+    handleSubmit,
+    
+    formState: { errors },
+  } = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data)
+        createUser(data.email,data.password)
+        .then(result =>{
+            const loggedUser = result.user;
+            console.log(loggedUser)
+        })
+    }
   return (
     <div className='flex justify-center items-center  mt-5'>
       <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl '>
@@ -54,7 +72,7 @@ const SignUp = () => {
 
             <span className='w-1/5 border-b dark:border-gray-400 lg:w-1/4'></span>
           </div>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className='mt-4'>
               <label
                 className='block mb-2 text-sm font-medium text-gray-600 '
@@ -64,11 +82,13 @@ const SignUp = () => {
               </label>
               <input
                 id='name'
+                {...register("name", { required: true })}
                 autoComplete='name'
                 name='name'
                 className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
                 type='text'
               />
+              {errors.name && <span className="text-red-500">Name field is required</span>}
             </div>
             
             <div className='mt-4'>
@@ -82,9 +102,11 @@ const SignUp = () => {
                 id='LoggingEmailAddress'
                 autoComplete='email'
                 name='email'
+                {...register("email", { required: true })}
                 className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
                 type='email'
               />
+              {errors.email && <span className="text-red-500">Email field is required</span>}
             </div>
 
             <div className='mt-4'>
@@ -101,9 +123,20 @@ const SignUp = () => {
                 id='loggingPassword'
                 autoComplete='current-password'
                 name='password'
+                {...register("password", { required: true,
+                 minLength:6,   
+                maxLength: 20 ,
+                 pattern:  /(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,20}/
+            })}
                 className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
                 type='password'
               />
+              {errors.password?.type === "required" && <span className="text-red-500">Password field is required</span>}
+
+              {errors.password?.type === "minLength" &&  <span className="text-red-500">Password must be 6 characters</span>}
+
+              {errors.password?.type === "pattern" &&  <span className="text-red-500">Password must One Uppercase One Lowercase One Special Characters and One Number</span>}
+              
             </div>
             <div className='mt-6'>
               <button
